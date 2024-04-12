dc7978e26559051d2ff94c1cf6d60bca54006522
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-04-12 01:13:09 -0400

    wip

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class UsersController : ControllerBase
+{
+    private readonly DataContext context;
+
+    public UsersController(DataContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpGet]
+    public async Task <ActionResult<IEnumerable<AppUser>>> GetUsers()  
+    {
+        var users = await context.Users.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id}")] 
+
+    public async Task <ActionResult<AppUser>> GetUser(int id) 
+    {
+        return await context.Users.FindAsync(id);
+    }
+}
