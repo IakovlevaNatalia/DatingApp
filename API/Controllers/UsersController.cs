411a41d4666222680c175b226ff6d6ae68ed900b
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-04-20 01:14:43 -0400

    created base ApiController

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,11 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-
-public class UsersController : ControllerBase
+public class UsersController : BaseApiController
 {
     private readonly DataContext context;
 
