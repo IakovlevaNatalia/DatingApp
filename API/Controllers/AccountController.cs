52349c8c76ff16bc32cd2d0ada892b7e4af957f5
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-04-20 23:14:43 -0400

    created an AccountController with a register endpoint

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext context;
+
+        public AccountController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost("register")] //Post: api/account/register
+        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        {
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+
+            };
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            return (user);
+        }
+
+    }
+}
