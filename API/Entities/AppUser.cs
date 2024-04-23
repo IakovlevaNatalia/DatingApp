474286f454e4580998ee08ac83bc85fb55696feb
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-04-22 23:29:27 -0400

    Added login endpoint

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,8 +1,12 @@
-﻿namespace API.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
+
+        [Required]
         public string UserName { get; set; }
 
         public byte[] PasswordHash { get; set; }
