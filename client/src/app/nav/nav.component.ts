a33107e443bfd44b41ba97f3a319f99658b842a9
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-06-13 21:51:58 -0400

    routing (66)

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -22,22 +23,21 @@ export class NavComponent implements OnInit {
   model: any = {}
   loggedIn=false;
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
+      next: _ => this.router.navigateByUrl('/members'),
       error: error=> console.log(error)
     })
   }
 
   logout () {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
