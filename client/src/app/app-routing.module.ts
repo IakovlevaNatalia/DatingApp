d8662d9af89243c64784daa2d89aa9de140f4b68
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-06-19 21:04:50 -0400

    Added an angular route guard (64)

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,10 +6,11 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailsComponent } from './members/member-details/member-details.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'members', component: MemberListComponent},
+  {path: 'members', component: MemberListComponent, canActivate: [authGuard]},
   {path: 'members/:id', component: MemberDetailsComponent},
   {path: 'lists', component: ListsComponent},
   {path: 'messages', component: MessagesComponent},
