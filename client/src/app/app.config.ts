822ca89d22f66e4fd6bd3fc1388ed85772ec37dc
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-06-26 22:41:08 -0400

    Added an error interceptor

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,16 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
+import { errorInterceptor } from './_interceptor/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideClientHydration(), 
-    provideHttpClient(withFetch()),
+    provideHttpClient(withInterceptors([errorInterceptor])),
     provideAnimations(),
     provideToastr({positionClass: 'toast-bottom-right'})
   ]
