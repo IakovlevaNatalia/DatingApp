7ae2b50f3ba8666bd7ee714c5a375bf2b6b087c8
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-06-28 21:51:40 -0400

    Added a DateTimeExtensions to CalculateAge (80)

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,5 +9,13 @@
         public DateOnly DateOfBirth { get; set; }
         public required string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public required string Gender { get; set; }
+        public string? Introduction { get; set; }
+        public string? Interests { get; set; }
+        public string? LookingFor { get; set; }
+        public string City { get; set; }
+        public required string Country { get; set; }
+        public List<Photo> Photo { get; set; }
     }
 }
---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace API;
+
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateOnly dob)
+        {
+            var today = DateOnly.FromDateTime(DateTime.Now);
+
+            var age = today.Year - dob.Year;
+
+            if (dob > today.AddYears(-age)) age--;
+
+            return age;
+        }
+    }
+
