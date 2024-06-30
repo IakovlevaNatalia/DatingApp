50a43a2112ba9b65e50d144cdcd5abdd4ead15b1
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-06-29 20:34:56 -0400

    Seeding data (84)

--- API/Program.cs
+++ API/Program.cs
@@ -38,4 +38,18 @@ app.UseAuthentication();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+
 app.Run();
