872d716d2a1181ca9cf05cce22dd0f0debbc5ca6
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-07-05 20:31:20 -0400

    Styled the member detailed page (105)

--- client/src/app/members/member-details/member-details.component.ts
+++ client/src/app/members/member-details/member-details.component.ts
@@ -2,12 +2,13 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 
 @Component({
   selector: 'app-member-details',
   standalone: true,
-  imports: [],
+  imports: [TabsModule],
   templateUrl: './member-details.component.html',
   styleUrl: './member-details.component.css'
 })
@@ -27,5 +28,4 @@ export class MemberDetailsComponent implements OnInit {
      next: member => this.member = member 
     })
   }
-
 }
