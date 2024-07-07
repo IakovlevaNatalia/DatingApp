c415cfaf1ff457429d95ea0ebb9e07fb93b6f387
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-07-07 11:56:20 -0400

    Added loading indicator (115)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,10 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
 import { HomeComponent } from "./home/home.component";
-import { RouterModule } from '@angular/router';
-import { ToastrModule } from 'ngx-toastr';
-import { HttpClient } from '@angular/common/http';
-import { User } from './user';
+import { NgxSpinnerComponent } from 'ngx-spinner';
+import { User } from './_models/user';
 
 
 @Component({
@@ -15,38 +13,21 @@ import { User } from './user';
   styleUrls: ['./app.component.css'],
   standalone: true,
   providers: [],
-  imports: [
-    NavComponent,
-    HomeComponent,
-    RouterModule,
-    ToastrModule,
-    RouterOutlet
+  imports: [RouterOutlet, NavComponent, HomeComponent, NgxSpinnerComponent
   ]
 })
 export class AppComponent implements OnInit {
-  http = inject (HttpClient);
   private accountService = inject(AccountService);
-  title = 'Dating app';
   users: any;
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
-  setCurrentUser() {
+    setCurrentUser() {
       const userString = localStorage.getItem('user');
       if(!userString) return;
       const user: User = JSON.parse(userString);
       this.accountService.setCurrentUser(user);
-    
+     }
   }
-
-  getUsers () {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users =response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
-  }
-}
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
@@ -7,13 +7,18 @@ import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
 import { errorInterceptor } from './_interceptors/error.interceptor';
 import { jwtInterceptor } from './_interceptors/jwt.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { loadingInterceptor } from './_interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideClientHydration(), 
-    provideHttpClient(withFetch(), withInterceptors([errorInterceptor, jwtInterceptor])),
+    provideHttpClient(withFetch(), withInterceptors([errorInterceptor, jwtInterceptor, loadingInterceptor])),
     provideAnimations(),
-    provideToastr({positionClass: 'toast-bottom-right'})
+    provideToastr({
+      positionClass: 'toast-bottom-right'
+    }),
+    importProvidersFrom(NgxSpinnerModule)
   ]
 };
