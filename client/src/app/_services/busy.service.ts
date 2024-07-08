614ca326f372d3d4e931465bbab5ec9f8e5552e1
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-07-07 22:20:22 -0400

    Using the service to store state (116)

--- client/src/app/_services/busy.service.ts
+++ client/src/app/_services/busy.service.ts
@@ -18,7 +18,7 @@ export class BusyService {
 }
 idle() {
   this.busyRequestCount--;
-  if(this.busyRequestCount <= 0) {
+  if (this.busyRequestCount <= 0) {
     this.busyRequestCount = 0;
     this.spinnerService.hide();
     }
