7ae2e1b381a4d5c7813faac43fd1486cd756b5c7
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-07-10 18:33:00 -0400

    Adding a photo service (122)

---
+++ API/Helpers/CloudinarySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Helpers
+{
+    public class CloudinarySettings
+    {
+        public required string CloudName { get; set; }
+        public required string ApiKey { get; set; }
+        public required string ApiSecret { get; set; }
+    }
+}
