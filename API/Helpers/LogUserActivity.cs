384b8f5a4361a41a6a038aacdd0c775f1c9518e7
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-08-02 22:03:50 -0400

    Added an action filter (159)

---
+++ API/Helpers/LogUserActivity.cs
@@ -0,0 +1,22 @@
+﻿using API.Extensions;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next();
+            if (context.HttpContext.User.Identity?.IsAuthenticated != true) return;
+
+            var username = resultContext.HttpContext.User.GetUsername();
+
+            var repo = resultContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
+            var user = await repo.GetUserByUsernameAsync(username);
+            if (user == null) return;
+            user.LastActive = DateTime.UtcNow;
+            await repo.SaveAllAsync();
+        }
+    }
+}
