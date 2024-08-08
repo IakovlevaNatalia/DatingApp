0ae603581cda522d7e317fdd2e2b3d77ca730867
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-08-08 13:48:26 -0400

    Added a LikesRepository (168)

---
+++ API/Data/LikesRepository.cs
@@ -0,0 +1,39 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+
+namespace API.Data
+{
+    public class LikesRepository : ILikesRepository
+    {
+        public void AddLike(UserLike like)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteLike(UserLike like)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<int>> GetCurrentUserLikeIds(int currentUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserLike> GetUserLike(int sourceUserId, int targetUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<MemberDto>> GetUserLikes(string predicate, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
