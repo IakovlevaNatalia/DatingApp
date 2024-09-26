0d31633a79ac4444286e9857b2c696fcf46d4394
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-09-26 14:49:36 -0400

    Adding roles to the app (203)

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -7,7 +7,7 @@ namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(UserManager<AppUser> userManager)
+        public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
         {
             if (await userManager.Users.AnyAsync()) return;
 
@@ -19,11 +19,36 @@ namespace API.Data
 
             if (users == null) return;
 
+            var roles = new List<AppRole>
+            {
+                new() { Name = "Member" },
+                new() { Name = "Admin" },
+                new() { Name = "Moderator" },
+            };
+
+            foreach (var role in roles)
+            {
+                await roleManager.CreateAsync(role);
+            }
+
             foreach (var user in users)
             {
                 user.UserName = user.UserName!.ToLower();
                 await userManager.CreateAsync(user, "Pa$$w0rd");
+                await userManager.AddToRoleAsync(user, "Member");
             }
+
+            var admin = new AppUser()
+            {
+                UserName = "admin",
+                KnownAs = "Admin",
+                Gender = "",
+                City = "",
+                Country = ""
+            };
+
+            await userManager.CreateAsync(admin, "Pa$$w0rd");
+            await userManager.AddToRolesAsync(admin, ["Admin", "Moderator"]);
         }
 
     }
