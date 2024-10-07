2fe3cb837a537c20698a7d3475c33b25d0ca22df
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-10-06 20:58:54 -0400

    Adding a presence tracker (220)

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -2,6 +2,7 @@
 using API.Helpers;
 using API.Interfaces;
 using API.Services;
+using API.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
@@ -26,6 +27,7 @@ namespace API.Extensions
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.Configure<CloudinarySettings>(config.GetSection(("CloudinarySettings")));
             services.AddSignalR();
+            services.AddSingleton<PresenceTracker>();
 
             return services;
         }
---
+++ API/SignalR/PresenceTracker.cs
@@ -0,0 +1,51 @@
+﻿namespace API.SignalR
+{
+    public class PresenceTracker
+    {
+        private static readonly Dictionary<string, List<string>> OnLineUsers = [];
+
+        public Task UserConnected(string username, string connectionId)
+        {
+            lock (OnLineUsers)
+            {
+                if (OnLineUsers.ContainsKey(username))
+                {
+                    OnLineUsers[username].Add(connectionId);
+                }
+                else
+                {
+                    OnLineUsers.Add(username, [connectionId]);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task UserDisconnected(string username, string connectionId)
+        {
+            lock (OnLineUsers)
+            {
+                if (!OnLineUsers.ContainsKey(username)) return Task.CompletedTask;
+
+                OnLineUsers[(username)].Remove(connectionId);
+
+                if (OnLineUsers[username].Count == 0)
+                {
+                    OnLineUsers.Remove(username);
+                }
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task<string[]> GetOnlineUsers()
+        {
+            string[] onlineUsers;
+            lock (OnLineUsers)
+            {
+                onlineUsers = OnLineUsers.OrderBy(k => k.Key).Select(k => k.Key).ToArray();
+            }
+
+            return Task.FromResult(onlineUsers);
+        }
+    }
+}
