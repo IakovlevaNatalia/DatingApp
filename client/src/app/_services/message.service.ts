c77d28b997fa2bc412b3bcc63598360839e0bfd6
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-10-16 11:59:19 -0400

    Sending message via the hub (226)

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -29,8 +29,11 @@ export class MessageService {
     this.hubConnection.start().catch(error => console.log(error));
 
     this.hubConnection.on('ReceiveMessageThread', messages => {
-      this.messageThread.set(messages);
+      this.messageThread.set(messages)
       
+    });
+    this.hubConnection.on('NewMessage', message => {
+      this.messageThread.update(messages => [...messages, message])
     })
   }
   stopHubConnection() {
@@ -54,8 +57,8 @@ export class MessageService {
     return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username);
   }
 
-  sendMessage(username: string, content: string) {
-    return this.http.post<Message>(this.baseUrl + 'messages', {recipientUsername: username, content})
+  async sendMessage(username: string, content: string) {
+    return this.hubConnection?.invoke('SendMessage', {recipientUsername: username, content})
   }
 
   deleteMessage (id: number) {
