f6b4e63c9cee56b6748c585cc688b89b047a5071
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-11-15 09:32:41 -0500

    Added a FallbackController (246)

--- API/Program.cs
+++ API/Program.cs
@@ -42,6 +42,7 @@ public class Program
         app.MapControllers();
         app.MapHub<PresenceHub>("hubs/presence");
         app.MapHub<MessageHub>("hubs/message");
+        app.MapFallbackToController("Index", "Fallback");
 
         using var scope = app.Services.CreateScope();
         var services = scope.ServiceProvider;
