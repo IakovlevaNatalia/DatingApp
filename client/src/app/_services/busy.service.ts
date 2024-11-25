88dbaa1e8579971e83e198c4f8f6caf30399a5f7
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-11-25 18:49:27 -0500

    Fixed whitespace issue

--- client/src/app/_services/busy.service.ts
+++ client/src/app/_services/busy.service.ts
@@ -16,6 +16,7 @@ export class BusyService {
     color: '#333333'
   })
 }
+
 idle() {
   this.busyRequestCount--;
   if (this.busyRequestCount <= 0) {
--- client/src/app/_services/likes.service.ts
+++ client/src/app/_services/likes.service.ts
@@ -9,7 +9,6 @@ import { setPaginatedResponse, setPaginationHeaders } from './paginationHelper';
   providedIn: 'root'
 })
 export class LikesService {
-
   baseUrl = environment.apiUrl;
   private http = inject(HttpClient);
   likeIds = signal<number[]>([]);
@@ -19,7 +18,7 @@ export class LikesService {
     return this.http.post(`${this.baseUrl}likes/${targetId}`, {})
   }
 
-  getLikes(predicate: string, pageNumber: number, pageSize:number) {
+  getLikes(predicate: string, pageNumber: number, pageSize: number) {
     let params = setPaginationHeaders(pageNumber, pageSize);
 
     params = params.append('predicate', predicate);
@@ -30,8 +29,7 @@ export class LikesService {
        })
   }
   
-  getLikeIds()
-  {
+  getLikeIds() {
     return this.http.get<number[]>(`$this.baseUrl}likes/list`).subscribe({
       next: ids => this.likeIds.set(ids)
     })
