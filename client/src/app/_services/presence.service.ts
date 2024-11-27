7bd04757ec14407c4e0e28061b91a078e1d17aab
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-11-27 09:43:20 -0500

    Fixed userIsOnline and userIsOffline

--- client/src/app/_services/presence.service.ts
+++ client/src/app/_services/presence.service.ts
@@ -6,7 +6,6 @@ import { User } from '../_models/user';
 import { Router } from '@angular/router';
 import { take } from 'rxjs';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -28,12 +27,12 @@ export class PresenceService {
       this.hubConnection.start().catch(error => console.log(error));
 
       this.hubConnection.on('UserIsOnline', username => {
-        this.toastr.warning(username + ' has connected');
-    });
+        this.onlineUsers.update(users => [...users, username]);
+      });
 
       this.hubConnection.on('UserIsOffline', username => {
-        this.toastr.warning(username + ' has disconnected');
-    })
+        this.onlineUsers.update(users => users.filter(x => x !== username));
+      });
 
       this.hubConnection.on('GetOnlineUsers', usernames => {
         this.onlineUsers.set(usernames);
