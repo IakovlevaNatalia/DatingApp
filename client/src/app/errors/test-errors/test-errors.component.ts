d171551c358acba7993689545bb498e183326217
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-11-30 10:08:47 -0500

    Fixed whitespace issue

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -20,12 +20,14 @@ export class TestErrorsComponent {
       error: error => console.log(error)
     })
   }
+
   get401Error() {
     this.http.get(this.baseUrl + 'buggy/auth').subscribe({
       next: response => console.log(response),
       error: error => console.log(error)
     })
   }
+
   get404Error() {
     this.http.get(this.baseUrl + 'buggy/not-found').subscribe({
       next: response => console.log(response),
@@ -39,6 +41,7 @@ export class TestErrorsComponent {
       error: error => console.log(error)
     })
   }
+
   get400ValidationError() {
     this.http.post(this.baseUrl + 'account/register', {}).subscribe({
       next: response => console.log(response),
@@ -48,4 +51,5 @@ export class TestErrorsComponent {
       }
     })
   }
+  
 }
