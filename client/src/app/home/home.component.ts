7c5dd572f8778c29ceb0d52d1ee20408b0485242
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-12-01 10:00:18 -0500

    Fixed whitespace issue in home.component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -9,12 +9,12 @@ import { RegisterComponent } from "../register/register.component";
     imports: [RegisterComponent]
 })
 export class HomeComponent {
-
   registerMode=false;
 
   registerToggle() {
     this.registerMode = !this.registerMode
   }
+  
   cancelRegisterMode(event: boolean) {
     this.registerMode = event;
   }
