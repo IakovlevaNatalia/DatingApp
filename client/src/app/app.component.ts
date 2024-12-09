e9870b76a140a05bfe0f4bbb900e186e7d7eca78
Natalia Balakov <69373349+IakovlevaNatalia@users.noreply.github.com>
2024-12-09 10:19:04 -0500

    Fixed app.component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,34 +1,28 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
 import { HomeComponent } from "./home/home.component";
 import { NgxSpinnerComponent } from 'ngx-spinner';
-import { User } from './_models/user';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  providers: [],
-  imports: [RouterOutlet, NavComponent, HomeComponent, NgxSpinnerComponent
-  ]
+  imports: [RouterOutlet, NavComponent, HomeComponent, NgxSpinnerComponent]
 })
 export class AppComponent implements OnInit {
-  private accountService = Inject(AccountService);
-  users: any;
+  private accountService = inject(AccountService);
 
   ngOnInit(): void {
     this.setCurrentUser();
    }
   
-
     setCurrentUser() {
       const userString = localStorage.getItem('user');
       if(!userString) return;
-      const user: User = JSON.parse(userString);
+      const user = JSON.parse(userString);
       this.accountService.setCurrentUser(user);
      }
   }
